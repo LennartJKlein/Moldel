89034ac65201f6a1b1b87eeeca24c43b1984a6e5
Lennart Klein <lennart.klein@eo.nl>
2023-01-09 08:40:24 +0100

    add exam and earning data of s24e1

--- moldel/Data/ExamData/Exams/Season24.py
+++ moldel/Data/ExamData/Exams/Season24.py
@@ -11,10 +11,91 @@ from ..Dataclasses.TestInput import TestInput, DelayedAnswer
 players1 = [Player.ANKE_24, Player.ANNICK_24, Player.DANIEL_24, Player.FROUKJE_24, Player.JURRE_24,
             Player.NABIL_24, Player.RANOMI_24, Player.SANDER_24, Player.SARAH_24, Player.SOY_24]
 questions1 = {
+    # De Mol is een...
+    1: Question({
+        # Man
+        1: [Player.DANIEL_24, Player.JURRE_24, Player.NABIL_24, Player.SANDER_24, Player.SOY_24],
+        # Vrouw
+        2: [Player.ANKE_24, Player.ANNICK_24, Player.FROUKJE_24, Player.RANOMI_24, Player.SARAH_24]
+    }),
+    # Met wie zat de Mol opgesloten in een kist?
+    10: Question({
+        # Anke
+        1: [Player.SANDER_24],
+        # Annick
+        2: [Player.SOY_24],
+        # Daniel
+        3: [Player.FROUKJE_24],
+        # Froukje
+        4: [Player.DANIEL_24],
+        # Jurre
+        5: [Player.SARAH_24],
+        # Nabil
+        6: [Player.RANOMI_24],
+        # Ranomi
+        7: [Player.NABIL_24],
+        # Sander
+        8: [Player.ANKE_24],
+        # Sarah
+        9: [Player.JURRE_24],
+        # Soy
+        10: [Player.ANNICK_24],
+    }),
+    # In welke kleur kist was de Mol opgesloten?
+    11: Question({
+        # Blauw
+        1: [Player.NABIL_24, Player.RANOMI_24],
+        # Geel
+        2: [Player.SANDER_24, Player.ANKE_24],
+        # Groen
+        3: [Player.JURRE_24, Player.SARAH_24],
+        # Rood
+        4: [Player.FROUKJE_24, Player.DANIEL_24],
+        # Zwart
+        5: [Player.SOY_24, Player.ANNICK_24]
+    }),
+    # Wat was de rol van de Mol tijdens de opdracht met het koor?
+    17: Question({
+        # Optreden
+        1: [Player.SOY_24, Player.SANDER_24, Player.ANKE_24, Player.SARAH_24],
+        # Deelopdrachten voltooien
+        2: [Player.NABIL_24,
+            Player.ANNICK_24, Player.DANIEL_24, Player.RANOMI_24, Player.FROUKJE_24]
+    }),
+    # Welk bedrag werd de Mol toebedeeld tijdens de opdracht met het koor?
+    18: Question({
+        # 250
+        1: [Player.ANKE_24],
+        # 500
+        2: [Player.SANDER_24],
+        # 750
+        3: [Player.JURRE_24],
+        # 1000
+        4: [Player.SOY_24],
+        # 1500
+        5: [Player.SARAH_24],
+        # De Mol deed deelopdrachten
+        6: [Player.NABIL_24,
+            Player.ANNICK_24, Player.DANIEL_24, Player.RANOMI_24, Player.FROUKJE_24]
+    }),
 }
 input1 = {
+    Player.NABIL_24: TestInput(
+        accusations=[Player.RANOMI_24, Player.SANDER_24, Player.FROUKJE_24, Player.ANKE_24]
+    ),
+    Player.DANIEL_24: TestInput({18: 6}),
+    Player.FROUKJE_24: TestInput({10: 4}, accusations=[Player.ANNICK_24, Player.DANIEL_24]),
+    Player.SARAH_24: TestInput({11: 4}, accusations=[Player.FROUKJE_24, Player.RANOMI_24, Player.DANIEL_24, Player.SANDER_24]),
+    Player.SANDER_24: TestInput(accusations=[Player.SOY_24, Player.FROUKJE_24, Player.SARAH_24, Player.ANNICK_24]),
+    Player.ANKE_24: TestInput({1: 2}, accusations=[Player.ANNICK_24, Player.FROUKJE_24,
+                                                   Player.NABIL_24, Player.SANDER_24, Player.SARAH_24, Player.SOY_24]),
+    Player.JURRE_24: TestInput(accusations=[Player.ANKE_24, Player.DANIEL_24,
+                                            Player.NABIL_24, Player.RANOMI_24, Player.SANDER_24, Player.SARAH_24, Player.SOY_24]),
+
+    Player.ANNICK_24: TestInput({17: 1}, accusations=[Player.FROUKJE_24, Player.RANOMI_24, Player.SARAH_24, Player.SANDER_24]),
+
 }
-result1 = None
+result1 = Result(DropType.EXECUTION_DROP, [Player.SARAH_24])
 episode1 = Episode(players1, result1, input1, questions1)
 
 ################
--- moldel/Data/MoneyData/Earnings/Season24.py
+++ moldel/Data/MoneyData/Earnings/Season24.py
@@ -8,6 +8,29 @@ from Data.Player import Player
 players1 = {Player.ANKE_24, Player.ANNICK_24, Player.DANIEL_24, Player.FROUKJE_24, Player.JURRE_24,
             Player.NABIL_24, Player.RANOMI_24, Player.SANDER_24, Player.SARAH_24, Player.SOY_24}
 exercises1 = [
+    # Opdracht 1: Gulden middenweg
+    # (250, 500, 1000, 2000, 4000)
+    # anke-sander sara-jurre soy-annick sander-anke nabil-ranomi daniel-froukje
+    # licht aan: sara-jurre soy-annick sander-anke nabil-ranomi daniel-froukje
+    # ontsnapt: sara-jurre sander-anke daniel-froukje
+    # aangekomen: sara-jurre sander-anke
+    Exercise(episode=1, alive=players1, maximum=4000, earned=[
+        Earning(money=500, major={Player.SARAH_24, Player.SANDER_24, Player.JURRE_24, Player.ANKE_24}),
+    ]),
+    # Opdracht 2: Toonaangevend
+    # muzikaal oor: froukje, ranomi, daniel, nabil, annick
+    # opgaan in muziek: sander, sara, jurre, anke, soy
+    # 1500 op Sara
+    # 1000 op jurre
+    # 100 anke
+    Exercise(episode=1, alive=players1, maximum=4000, earned=[
+        Earning(money=1500, major={Player.SARAH_24}, minor={Player.NABIL_24,
+                Player.ANNICK_24, Player.DANIEL_24, Player.RANOMI_24, Player.FROUKJE_24}),
+        Earning(money=1000, major={Player.SOY_24}, minor={Player.NABIL_24,
+                Player.ANNICK_24, Player.DANIEL_24, Player.RANOMI_24, Player.FROUKJE_24}),
+        Earning(money=250, major={Player.ANKE_24}, minor={Player.NABIL_24,
+                Player.ANNICK_24, Player.DANIEL_24, Player.RANOMI_24, Player.FROUKJE_24}),
+    ]),
 ]
 
 season24 = Season(
